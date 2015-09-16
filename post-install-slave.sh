8d6b3b1bdde9fc00168aac1c7eada8f95b818995
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2015-09-16 10:15:33 +0100

    Replace hardcoded path by a new environment variable

--- post-install-slave.sh
+++ post-install-slave.sh
@@ -8,17 +8,7 @@
 ##
 #set -x # DEBUG
 
-YUM=$(which yum)
-APT_GET=$(which apt-get)
-
 # Copy files sent through Jenkins into ~/.brooklyn folder
 mkdir -p ~/.brooklyn
-cp /var/tmp/*.* ~/.brooklyn/
-if [ ! -z $YUM ]; then
-        sudo chown -R ec2-user:ec2-user ~/.brooklyn/
-elif [ ! -z $APT_GET ]; then
-        sudo chown -R ubuntu:ubuntu ~/.brooklyn/
-else
-        echo "Error: cannot figure out the default user / group"
-        exit 1;
-fi
+cp $JENKINS_ROOT/*.* ~/.brooklyn/
+chmod 600 ~/.brooklyn/brooklyn.properties
