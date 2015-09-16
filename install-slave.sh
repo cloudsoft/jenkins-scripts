e7f2e6b305f151870ee67fb46c4e4bc9ceee3e9b
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2015-09-16 10:53:27 +0100

    Update RVM installation reload to match the requirements

--- install-slave.sh
+++ install-slave.sh
@@ -27,7 +27,7 @@ fi
 # Install RVM
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
+source ~/.rvm/scripts/rvm
 
 # Install ruby 2.2.* and rubygems through RVM
 rvm install 2.2
